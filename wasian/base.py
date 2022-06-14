31c8ad2fe5aa8b79dda7005c08e5a2dd03635093
feliciss <feliciss@users.noreply.github.com>
2022-06-14 11:16:47 +0000

    ✅ Ready to clone and code.

--- wasian/base.py
+++ wasian/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+wasian base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the wasian project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
@@ -14,4 +14,4 @@ and then choose `flask` as template.
 """
 
 # example constant variable
-NAME = "project_name"
+NAME = "wasian"
